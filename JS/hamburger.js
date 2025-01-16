727cb0c6fbb7631d1e85bddb882a6bf05bc5481a
hamzaamjad860 <128701040+hamzaamjad860@users.noreply.github.com>
2025-01-16 22:59:56 +0500

    Add files via upload

---
+++ JS/hamburger.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navMenu = document.getElementById('nav-menu');
+    nav-Menu.classList.toggle('show'); // Toggle the 'show' class
+});
